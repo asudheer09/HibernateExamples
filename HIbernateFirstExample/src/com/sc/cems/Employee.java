c917bfca9c5eb9b36d2e52e4dbdaca550a9bb92f
sudheer <sudheer@sudheer-HP-Notebook>
2019-03-26 20:24:52 +0530

    commiting the hibernate first example.

---
+++ HIbernateFirstExample/src/com/sc/cems/Employee.java
@@ -0,0 +1,31 @@
+package com.sc.cems;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+public class Employee {
+	private int id;
+	private String firstName;
+	private String lastName;
+	
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getFirstName() {
+		return firstName;
+	}
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+	public String getLastName() {
+		return lastName;
+	}
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+	
+}
